4293e0a8a17de1aeaf0ebd410719113e9d698b40
pc223 <10551242+pc223@users.noreply.github.com>
2021-08-11 18:19:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import sys
 print(sys.path)
 import platform
 print(platform.platform())
+
+import requests
+
+
+r = requests.get('https://google.com')
+print(r.title)
+
+import urllib.request
+
+local_filename, headers = urllib.request.urlretrieve("https://www.python.org/ftp/python/3.8.9/python-3.8.9-embed-amd64.zip", "./")
+
+print(local_filename, headers, sep='   |   ')
+
+
